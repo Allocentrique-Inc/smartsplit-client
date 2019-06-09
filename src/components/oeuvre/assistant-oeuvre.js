60955017e71db39e370ed1b4289762739b216cf6
Vincent de Grandpré <vincent@de-grandpre.quebec>
2019-06-09 18:37:21 -0400

    Internationalisation des libellés des boutons des assistants.

--- src/components/oeuvre/assistant-oeuvre.js
+++ src/components/oeuvre/assistant-oeuvre.js
@@ -11,24 +11,33 @@ import PageParoles from './assistant-oeuvre-paroles'
 import PageGenres from './assistant-oeuvre-genres'
 import PagePro from './assistant-oeuvre-pro'
 
+// Traduction
+import { Translation } from 'react-i18next'
+
 const AssistantOeuvre = ({onSubmit}) => (
-    <Wizard        
-        onSubmit={onSubmit}
-    >
-        <Wizard.Page>            
-            <PageDescription /> 
-        </Wizard.Page>
-        <Wizard.Page>
-            <PageParoles />
-        </Wizard.Page>
-        <Wizard.Page>
-            <PageGenres />
-        </Wizard.Page>
-        <Wizard.Page>
-            <PagePro />
-        </Wizard.Page>
-                
-  </Wizard>
+    <Translation>
+        {
+            (t) =>
+                <Wizard        
+                        onSubmit={onSubmit}
+                        buttonLabels={{previous: t('navigation.precedent'), next: t('navigation.suivant'), submit: t('navigation.envoi')}}
+                    >
+                        <Wizard.Page>            
+                            <PageDescription /> 
+                        </Wizard.Page>
+                        <Wizard.Page>
+                            <PageParoles />
+                        </Wizard.Page>
+                        <Wizard.Page>
+                            <PageGenres />
+                        </Wizard.Page>
+                        <Wizard.Page>
+                            <PagePro />
+                        </Wizard.Page>
+                                
+                </Wizard>
+        }
+    </Translation>    
 )
 
 export default AssistantOeuvre
