a226ffd543d9696c102ead5e334c3ac72e7860d9
David Wawryko <dwawryko@gmail.com>
2019-07-30 16:17:57 -0400

    Updated register and login components, added password strength meter

--- src/components/auth/Welcome.js
+++ src/components/auth/Welcome.js
@@ -3,33 +3,22 @@ import { Translation } from 'react-i18next'
 
 const Welcome = () => (
   
-  // <React.Fragment>
     <Translation>
       {
         (t) =>
         <section className="section auth">
          <div className="container">
-          {/* <tbody> */}
-            <tr>
-              <td>
-                <h2>{t('auth.bienvenue.titre')}</h2>
-
-                <p>
-                  {t('auth.bienvenue.preambule')}
-                </p>
-
-                <p>
-                  {t('auth.bienvenue.indication')}
-                </p>
-
-              </td>
-            </tr>
-          {/* </tbody>     */}
+            <h2>{t('auth.bienvenue.titre')}</h2>
+            <p>
+              {t('auth.bienvenue.preambule')}
+            </p>
+            <p>
+              {t('auth.bienvenue.indication')}
+            </p>
          </div>
       </section>
       }      
     </Translation>    
-  // </React.Fragment>
 )
 
 export default Welcome
