f696229fbc87a7626cad4fadf07fdfc9bcfce3c8
CaribouFuté <frederic.chiasson@gmail.com>
2019-08-13 19:25:24 -0400

    Fait nouvel en-tête

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import '../../assets/scss/navbar.scss';
+import placeholder from '../../assets/images/placeholder.png';
 
 export class Navbar extends React.Component {
     render() {
         return (
             <div className="Navbar">
                 <div className="left">
-                    Left
+                    <div className="song-image">
+                        <img src={ placeholder }/>
+                    </div>
+
+                    <div className="songName">
+                        [Nom de chanson]
+                    </div>
+
+                    <div className="documentation-label">
+                        Documentation
+                    </div>
                 </div>
 
                 <div className="right">
-                    Right
+                    <div className="save-and-quit-label">
+                        <a href="#">
+                            Enregistrer et quitter
+                        </a>
+                    </div>
                 </div>
             </div>
         );
