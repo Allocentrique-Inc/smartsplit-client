16f2ed4bbb590ce5e2b95d99933705c31ebf07ec
Frédéric Chiasson <frederic.chiasson@gmail.com>
2019-08-22 11:49:39 -0400

    Commencé formulaire instrument

--- src/components/formulaires/champ-instrument.js
+++ src/components/formulaires/champ-instrument.js
@@ -4,12 +4,20 @@ import { ItemSelectionne } from "./item-selectionne";
 export class ChampInstrument extends Component {
     render() {
         return (
-            <ItemSelectionne
-                key={ this.props.item.key }
-                image={ this.props.item.image.src }
-                nom={ this.props.item.text }
-                onClick={ this.props.onClick }
-            />
+            <>
+                <ItemSelectionne
+                    key={ this.props.item.key }
+                    image={ this.props.item.image.src }
+                    nom={ this.props.item.text }
+                    onClick={ this.props.onClick }
+                />
+
+                <div className="instrument-form">
+                    <p>Quel type d'interprétation ?</p>
+
+                    <p>Quel rôle dans la pièce musicale ?</p>
+                </div>
+            </>
         );
     }
 }
