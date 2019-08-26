f310f0ee1991ba66e931f4cf1777767258cb4f29
Vincent de Grandpré <vincent@de-grandpre.quebec>
2019-08-26 10:57:30 -0400

    retrait du rafraichissement de trop

--- src/components/partage/partage-sommaire.js
+++ src/components/partage/partage-sommaire.js
@@ -428,10 +428,7 @@ export default class SommairePartage extends Component {
                 if((!this.estVoteFinal() && this.estVoteClos()) || this.state.rafraichir){
                     this.rafraichissementAutomatique()
                     if(this.estVoteFinal()){
-                        // C'était le dernier rafraichissement (p.ex. cas où le dernier vote entre)
-                        this.rafraichirDonnees(()=>{
-                            window.location.reload()
-                        })
+                        // C'était le dernier rafraichissement (p.ex. cas où le dernier vote entre)                        
                         this.setState({rafraichir: false})
                     }
                 }
