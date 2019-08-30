d66d66991cfc1d24c7b1f9b16706e94e0b058d03
CaribouFuté <frederic.chiasson@gmail.com>
2019-08-30 15:48:07 -0400

    Ajouté le reste des bons icônes dans les libellés de page

--- src/components/oeuvre/page-liens.js
+++ src/components/oeuvre/page-liens.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Translation } from "react-i18next";
 import { PageAssistant } from '../canevas/page-assistant';
 import { ChampDate } from "../formulaires/champ-date";
-import LinkOrange from '../../assets/svg/icons/link-orange.svg';
-import LinkGreen from '../../assets/svg/icons/link-green.svg';
+import LinkCircleOrange from '../../assets/svg/icons/link-circle-orange.svg';
+import LinkCircleGreen from '../../assets/svg/icons/link-circle-green.svg';
 
 export default class PageLiens extends React.Component {
 
     icon() {
-        return this.props.pochette ? LinkOrange : LinkGreen;
+        return this.props.pochette ? LinkCircleOrange : LinkCircleGreen;
     }
 
     render() {
--- src/components/oeuvre/page-paroles.js
+++ src/components/oeuvre/page-paroles.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Translation } from "react-i18next";
 import { PageAssistant } from '../canevas/page-assistant';
 import { ChampDate } from "../formulaires/champ-date";
-import PlusCircleOrange from '../../assets/svg/icons/plus-circle-orange.svg';
-import PlusCircleGreen from '../../assets/svg/icons/plus-circle-green.svg';
+import LyricsCircleOrange from '../../assets/svg/icons/lyrics-circle-orange.svg';
+import LyricsCircleGreen from '../../assets/svg/icons/lyrics-circle-green.svg';
 
 export default class PageParoles extends React.Component {
 
     icon() {
-        return this.props.pochette ? PlusCircleOrange : PlusCircleGreen;
+        return this.props.pochette ? LyricsCircleOrange : LyricsCircleGreen;
     }
 
     render() {
