bae75f533869ed0237d4532a06f56a2521998a92
CaribouFuté <frederic.chiasson@gmail.com>
2019-09-06 11:07:46 -0400

    Ajouté champ streaming dans page liens

--- src/components/oeuvre/page-liens.js
+++ src/components/oeuvre/page-liens.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Translation } from "react-i18next";
 import Page from '../page-assistant/page';
 import { ChampDate } from "../page-assistant/champ-date";
 import LinkCircleOrange from '../../assets/svg/icons/link-circle-orange.svg';
 import LinkCircleGreen from '../../assets/svg/icons/link-circle-green.svg';
 import Colonne from "../page-assistant/colonne";
 import Entete from "../page-assistant/entete";
+import TitreChamp from "../page-assistant/titre-champ";
+import SpotifyIcon from '../../assets/svg/icons/spotify.svg';
+import GooglePlayIcon from '../../assets/svg/icons/googleplaymusic.svg';
+import { Translation } from 'react-i18next'
+import ChampStreaming from "../page-assistant/champ-streaming";
+import DotIcon from '../../assets/svg/icons/dot.svg';
+import ChampTexte from "../page-assistant/champ-texte";
 
 export default class PageLiens extends React.Component {
 
+    streamingApps = [
+        {
+            icon: SpotifyIcon,
+            label: 'Spotify'
+        },
+        {
+            icon: GooglePlayIcon,
+            label: 'Google Play',
+        },
+        {
+            icon: null,
+            label: 'Pandora',
+        }
+    ];
+
     icon() {
         return this.props.pochette ? LinkCircleOrange : LinkCircleGreen;
+    }
+
+    renderStreamingFields() {
+        return this.streamingApps.map(app => (
+            <ChampStreaming
+                icon={ app.icon || DotIcon }
+                label={ app.label }
+                placeholder={ 'Coller un lien...' }
+            />)
+        );
     }
 
     render() {
@@ -29,6 +60,12 @@ export default class PageLiens extends React.Component {
                                     question={ 'La pièce musicale est-elle déjà diffusée?' }
                                     description={ 'Pour augmenter les chances que ta pièce soit découverte et écoutée, documente ses liens d’écoute et de vente en ligne.' }
                                 />
+
+                                <TitreChamp
+                                    label={ this.props.values.title + ' sur...' }
+                                />
+
+                                { this.renderStreamingFields() }
                             </Colonne>
                         </Page>
                 }
