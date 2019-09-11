e0452630c01e22278c72c5f0c54c078bbb3743c1
David Wawryko <david@iptoki.com>
2019-09-11 11:08:53 -0400

    Readded declaration modal stub

--- src/index.js
+++ src/index.js
@@ -36,8 +36,9 @@ import Troissplits from './components/visualisation/partage/troissplits'
 // Composantes auth
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
-import Register2 from './components/auth/Register-2'
+import ModifyUser from './components/auth/ModifyUser'
 import Register3 from './components/auth/Register-3'
+import Declaration from './components/auth/Declaration'
 import ForgotPassword from './components/auth/ForgotPassword'
 import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification'
 import ChangePasswordVerification from './components/auth/ChangePasswordVerification'
@@ -87,7 +88,8 @@ const renderRoutes = () => {
             <Route exact path="/liste-oeuvres" component={ListeOeuvres} />
             <Route exact path="/login" component={renderLogin} />
             <Route exact path="/register" component={Register} />
-            <Route exact path="/register-2" component={Register2} />
+            <Route exact path="/register-2" component={ModifyUser} />
+            <Route exact path="/declaration" component={Declaration} />
             <Route exact path="/register-3" component={Register3} />
             <Route exact path="/sign-in-facebook" component={SignInFacebook} />
             <Route exact path="/sign-in-google" component={SignInGoogle} />
