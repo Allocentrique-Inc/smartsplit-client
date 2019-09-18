5929fc60a8cf656d8b3ea4f1f0b755d3c0902173
CaribouFuté <frederic.chiasson@gmail.com>
2019-09-18 10:30:59 -0400

    Enlevé mode debug

--- src/components/oeuvre/assistant-oeuvre.js
+++ src/components/oeuvre/assistant-oeuvre.js
@@ -209,7 +209,7 @@ class AssistantOeuvre extends Component {
                                         submit: t('navigation.envoi')
                                     } }
 
-                                    debug={ true }
+                                    debug={ false }
                                 >
                                     <Wizard.Page>
                                         <PageCreation
