046ae5bb9b83a157a41adc2df2818f5b163d15e8
Lila Ait <lila.ait.ab@gmail.com>
2019-09-24 11:26:05 -0400

    Changements Modals

--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 // import FormErrors from "../FormErrors";
 // import Validate from "../utility/FormValidation";
 import { Auth } from "aws-amplify";
+import { Translation } from "react-i18next";
 
-import { withTranslation } from "react-i18next";
+//import { withTranslation } from "react-i18next";
 
-import { Connexion } from "./Connexion.js";
+const TYPE_LOGIN = 0,
+  TYPE_REGISTER = 1,
+  TYPE_FORGOT = 2;
 
 class ForgotPassword extends Component {
   state = {
@@ -54,42 +57,36 @@ class ForgotPassword extends Component {
   };
 
   render() {
-    const { t } = this.props;
+    //const { t } = this.props;
+
+    /*{!this.state.patience && (
+            <span className="top-login">
+              <div
+                onClick={() => {
+                  // Le paramètre de la fonction afficher est le TYPE_ dans le fichier Connexion.js
+                  this.props.parent.afficher(0);
+                }}
+                style={{ color: "#2DA84F", cursor: "pointer" }}
+              >
+                {t("entete.inscription")}
+              </div>
+            </span>
+        )}{!this.state.patience && (
+            <span className="top-register">
+              <div
+                onClick={() => {
+                  // Le paramètre de la fonction afficher est le TYPE_ dans le fichier Connexion.js
+                  this.props.parent.afficher(1);
+                }}
+                style={{ color: "#2DA84F", cursor: "pointer" }}
+              >
+                {t("entete.inscription")}
+              </div>
+            </span>
+        )}*/
     return (
-      <React.Fragment>
-        <div className="topRegisterLogin">
-          {!this.state.patience && (
-            <div>
-              <span className="top-login">
-                <div
-                  onClick={() => {
-                    // Le paramètre de la fonction afficher est le TYPE_ dans le fichier Connexion.js
-                    this.props.parent.afficher(0);
-                  }}
-                  style={{ color: "#2DA84F", cursor: "pointer" }}
-                >
-                  {t("entete.inscription")}
-                </div>
-              </span>
-            </div>
-          )}
-          {!this.state.patience && (
-            <div>
-              <span className="top-register">
-                <div
-                  onClick={() => {
-                    // Le paramètre de la fonction afficher est le TYPE_ dans le fichier Connexion.js
-                    this.props.parent.afficher(1);
-                  }}
-                  style={{ color: "#2DA84F", cursor: "pointer" }}
-                >
-                  {t("entete.inscription")}
-                </div>
-              </span>
-            </div>
-          )}
-        </div>
-        <section className="section auth">
+      <Translation className="section auth">
+        {t => (
           <div className="container">
             <h1>{t("auth.oublier.titre")}</h1>
             <p>{t("auth.oublier.preambule")}</p>
@@ -119,15 +116,17 @@ class ForgotPassword extends Component {
               </div>
               <div className="field">
                 <p className="control">
-                  <button className="button is-success">Submit</button>
+                  <button className="button is-success">
+                    {t("collaborateur.attribut.bouton.soumettre")}
+                  </button>
                 </p>
               </div>
             </form>
           </div>
-        </section>
-      </React.Fragment>
+        )}
+      </Translation>
     );
   }
 }
 
-export default withTranslation(ForgotPassword);
+export default ForgotPassword;
