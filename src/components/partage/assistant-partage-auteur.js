4c113d31e5193c3543ad9d4a09352f82cf890936
Lila Ait <lila.ait.ab@gmail.com>
2019-11-19 11:52:22 -0500

    Fusion Lili

--- src/components/partage/assistant-partage-auteur.js
+++ src/components/partage/assistant-partage-auteur.js
@@ -554,6 +554,7 @@ class PageAssistantPartageAuteur extends Component {
                                                                                                                         <Lock actif={this.state.partsInvariables[index] || this.props.values.droitAuteur.length === 1} />
                                                                                                                     </div>
                                                                                                                     <div className="ui ten wide column">
+                                                                                                                       
                                                                                                                         <ChampGradateurAssistant
                                                                                                                             changement={(id, delta) => {
                                                                                                                                 // Permet le changment seulement si personne ne sera à zéro
@@ -573,6 +574,7 @@ class PageAssistantPartageAuteur extends Component {
                                                                                                                                 )
                                                                                                                             }
                                                                                                                         />
+                                                                                                                        
                                                                                                                     </div>
 
                                                                                                                     <div className="ui four wide column">
