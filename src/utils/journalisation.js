36008de195f6f0eaf99ffaae2ff9a802133ad5f5
Vincent de Grandpré <vincent@de-grandpre.quebec>
2020-01-30 09:28:49 -0500

    Ajout de la ligne et du fichier source dans la journalisation

--- src/utils/journalisation.js
+++ src/utils/journalisation.js
@@ -1,4 +1,7 @@
 import winston from 'winston'
+import path from 'path'
+
+var RACINE = path.join(__dirname, '..')
 
 /**
  * Journalisation applicative.
@@ -26,43 +29,95 @@ export default class Journalisation {
         )
         
         this.moteur = winston.createLogger({
-            level: process.env.REACT_APP_LOGLEVEL || "info",
-            prettyPrint: true,
+            level: process.env.REACT_APP_LOGLEVEL || "info",            
             transports: [
-                new (winston.transports.Console)({format: couleursEtHorodatage})
-            ]
-        })
+                new (winston.transports.Console)(
+                    {
+                        format: couleursEtHorodatage, 
+                        prettyPrint: true,
+                        handleExceptions: true,
+                        json: true
+                    })
+            ],
+            exitOnError: false
+        })        
 
-        this.silly = (nom, msg) => {
-            this.moteur.silly(msg, {label: nom})
-        }
+    }
 
-        this.debug = (nom, msg) => {
-            this.moteur.debug(msg, {label: nom})
-        }
+    silly (nom, msg) {
+        nom = this.formatLogArguments(arguments)
+        this.moteur.silly(msg, {label: nom})
+    }
 
-        this.verbose = (nom, msg) => {
-            this.moteur.verbose(msg, {label: nom})
-        }
+    debug (nom, msg) {
+        nom = this.formatLogArguments(arguments)
+        this.moteur.debug(msg, {label: nom})
+    }
 
-        this.http = (nom, msg) => {
-            this.moteur.http(msg, {label: nom})
-        }
+    verbose (nom, msg) {
+        nom = this.formatLogArguments(arguments)
+        this.moteur.verbose(msg, {label: nom})
+    }
 
-        this.info = (nom, msg) => {
-            this.moteur.info(msg, {label: nom})
-        }
-    
-        this.warn = (nom, msg) => {
-            this.moteur.warn(msg, {label: nom})
-        }
-    
-        this.error = (nom, msg) => {            
-            this.moteur.error(msg, {label: nom})
-            if(msg.stack)
-                this.moteur.error(msg.stack, {label: `${nom}-trace`})
-        }        
+    http (nom, msg) {
+        nom = this.formatLogArguments(arguments)
+        this.moteur.http(msg, {label: nom})
+    }
 
+    info (nom, msg) {
+        nom = this.formatLogArguments(arguments)
+        this.moteur.info(msg, {label: nom})
+    }
+
+    warn (nom, msg) {
+        nom = this.formatLogArguments(arguments)
+        this.moteur.warn(msg, {label: nom})
+    }
+
+    error (nom, msg) {            
+        let nomDerive = this.formatLogArguments(arguments)
+        this.moteur.error(msg, {label: nomDerive})
+        if(msg.stack)
+            this.moteur.error(msg.stack, {label: `${nom}-trace`})
+    }        
+
+    /**
+     * Tente d'ajouter le fichier et le numéro de ligne de l'événement     
+     */
+    formatLogArguments (args) {
+        args = Array.prototype.slice.call(args)
+        var stackInfo = this.getStackInfo(1)
+        if (stackInfo) {
+            // Chemin du fichier relatif au projet
+            var calleeStr = stackInfo.relativePath + ':' + stackInfo.line
+            args[0] = `${args[0]} ${calleeStr}`
+        }
+        return [args[0]]
+    }
+
+    /**
+     * Parses and returns info about the call stack at the given index.
+     */
+    getStackInfo (stackIndex) {
+        // Analyse de la pile d'exécution
+        // Extraction du fichier, de la ligne et de la méthode
+        var stacklist = (new Error()).stack.split('\n').slice(3)        
+        // http://code.google.com/p/v8/wiki/JavaScriptStackTraceApi
+        // Attention à ne par modifier l'expression régulière (bogue NodeJS)
+        var stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/gi
+        var stackReg2 = /at\s+()(.*):(\d*):(\d*)/gi
+        var s = stacklist[stackIndex] || stacklist[0]
+        var sp = stackReg.exec(s) || stackReg2.exec(s)
+        if (sp && sp.length === 5) {
+            return {
+            method: sp[1],
+            relativePath: path.relative(RACINE, sp[2]),
+            line: sp[3],
+            pos: sp[4],
+            file: path.basename(sp[2]),
+            stack: stacklist.join('\n')
+            }
+        }
     }
 
     static getInstance() {
