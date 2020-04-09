c22d1bd1f0f2415f7ca641bb2cf69c7e792cd299
Guillaume Couture <gcouture@ngagestudio.com>
2020-04-09 16:45:44 -0400

    Page pour logout

---
+++ src/pages/auth/Logout/Logout.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {View} from "react-native"
+import AccessControl from "../../../widgets/AccessControl"
+
+export default function Logout() {
+	return <View>
+		<AccessControl redirectToLogin>
+		</AccessControl>
+	</View>
+}
