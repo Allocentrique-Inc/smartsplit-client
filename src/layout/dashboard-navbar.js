ab5dbf3f42337ee277d6ded4fe576adef55e5b9a
Pandassi <lila.ait.ab@gmail.com>
2020-05-05 10:00:18 -0400

    modale identifiant et corrections diverses

---
+++ src/layout/dashboard-navbar.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { useTranslation } from "react-i18next"
+import { useHistory } from "react-router-dom"
+import { View, BackHandler } from "react-native"
+import { Platform } from "../platform"
+import { Flex, Row, Hairline, Column, Group, Spacer } from "../layout"
+import { Heading } from "../text"
+import Button from "../widgets/button"
+import ArrowLeft from "../svg/arrow-left"
+import UserAvatar from "../smartsplit/user/avatar"
+
+export default function DashboardNavbar(props) {
+	const { header } = props
+	const [t] = useTranslation()
+	const history = useHistory()
+	const goBack = () => history.push("/dashboard")
+	return (
+		<>
+			{Platform.web && (
+				<>
+					<Row
+						of="group"
+						size="xlarge"
+						style={{
+							alignItems: "center",
+							maxWidth: 624,
+							alignSelf: "left",
+						}}
+					>
+						<View style={{ width: 223 }}>
+							<ArrowLeft onBack={goBack} />
+						</View>
+						<UserAvatar initials="XX" size="medium" />
+						<Heading level="5">{header}</Heading>
+						<Flex />
+					</Row>
+					<Hairline />
+					<Spacer of="group" />
+				</>
+			)}
+
+			{Platform.native && (
+				<>
+					<Row
+						of="component"
+						style={{
+							alignItems: "center",
+							alignSelf: "left",
+							maxWidth: 375,
+						}}
+					>
+						<ArrowLeft onBack={(onBack = { goBack })} />
+						<Heading level="4">{header}</Heading>
+						<Flex />
+						<Button tertiary text={t("general:buttons.save")} />
+					</Row>
+
+					<Hairline />
+					<Spacer of="group" />
+				</>
+			)}
+		</>
+	)
+}
---
+++ src/pages/dashboard/account/add-username.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react"
+import { useTranslation } from "react-i18next"
+import { Platform } from "../../../platform"
+import { DialogModal } from "../../../widgets/modal"
+import { Modal } from "../../../widgets/modal"
+import Button from "../../../widgets/button"
+import { Section, Column, Row, Group, Flex } from "../../../layout"
+import { TextField } from "../../../forms"
+import { Dropdown } from "../../../widgets/dropdown"
+import { Text, Heading, Paragraph } from "../../../text"
+import { Colors } from "../../../theme"
+import Scrollable from "../../../widgets/scrollable"
+
+export default function AddUsernameModal(props) {
+	const [t] = useTranslation()
+
+	const buttonSize = Platform.OS === "web" ? "medium" : "large"
+	const [canSubmit, setCanSubmit] = useState("")
+
+	const handleSubmit = () => {
+		if (!canSubmit) return false
+	}
+
+	const UsernameNO = null
+
+	return (
+		<Scrollable>
+			<DialogModal
+				visible={props.visible}
+				onRequestClose={props.onRequestClose}
+				title={t("username:add")}
+				buttons={
+					<>
+						<Button
+							text={t("general:buttons.cancel")}
+							tertiary
+							onClick={props.onRequestClose}
+							size={buttonSize}
+							style={Platform.OS !== "web" && { flex: 1 }}
+						/>
+						<Button
+							text={t("general:buttons.add")}
+							disabled={!canSubmit}
+							onClick={handleSubmit}
+							size={buttonSize}
+							style={Platform.OS !== "web" && { flex: 1 }}
+						/>
+					</>
+				}
+			>
+				<Group
+					of="group"
+					style={
+						Platform.OS === "web" && { minWidth: 560, alignSelf: "center" }
+					}
+				>
+					<Paragraph>{t("username:description")}</Paragraph>
+					<Dropdown
+						label={t("forms:labels.managingCie")}
+						placeholder={t("forms:placeholders.selectCie")}
+						value={UsernameNO}
+					/>
+				</Group>
+			</DialogModal>
+		</Scrollable>
+	)
+}
