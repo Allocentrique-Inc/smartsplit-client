4954982c878d8ee89132093b1a30a657757c8b73
Maxime Poulin <maxime@iptoki.com>
2020-05-30 00:11:20 -0400

    Réécriture partielle de la composante PhoneNumberField
    S'assurer que le numéro envoyé à onChangeText soit toujours un numéro au format international, et que l'affichage est tojours au format formatté sans être dérangeant pour les utilisateurs.
    
    La gestion des erreurs fonctionne maintenant même quand il y a conflit d'erreurs
    
    Ajoute aussi l'icone de status du numéro

--- src/forms/phone-number.js
+++ src/forms/phone-number.js
@@ -1,47 +1,91 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import TextField from "./text"
 import { parsePhoneNumberFromString } from "libphonenumber-js"
 import { useTranslation } from "react-i18next"
 
+import MoreHorizontal from "../../assets/svg/more-horizontal.svg"
+import CheckMark from "../svg/check-mark"
+import { Colors } from "../theme"
+
+export const StatusIcon = {
+	unverified: <MoreHorizontal />,
+	pending: <MoreHorizontal />,
+	active: <CheckMark />,
+	verified: <CheckMark color={Colors.action} />,
+}
+
 export function PhoneNumberField(props) {
-	const { value, onChangeText, ...nextProps } = props
-	const [isValid, setIsValid] = useState(false)
-	const [error, setError] = useState(null)
+	const { value, onChangeText, status, after, ...nextProps } = props
+
 	const { t } = useTranslation()
 
-	function handleOnBlur() {
-		const parsedPhone = parsePhoneNumberFromString(value || "", "CA")
-		setError(parsedPhone ? null : t("errors:invalidPhoneNumber"))
-		setIsValid(!!parsedPhone)
-		onChangeText(parsedPhone ? parsedPhone.number : "")
-	}
+	const [currentValue, setCurrentValue] = useState(value || "")
+	const [fieldValue, setFieldValue] = useState(toDisplayNumber(value) || "")
+	const [isFocused, setIsFocused] = useState(false)
+	const [error, setError] = useState(null)
 
-	function handleOnFocus() {
-		if (isValid) {
-			setIsValid(false)
-			onChangeText(toDisplayNumber(value))
-		}
-		if (error) {
-			setError(null)
-		}
-	}
+	const handleChange = useCallback(
+		(number) => {
+			setFieldValue(number)
 
-	function toDisplayNumber(phoneNumber) {
-		const parsedPhone = parsePhoneNumberFromString(phoneNumber, "CA")
-		return parsedPhone.formatNational()
-	}
+			const parsed = toInternational(number)
+
+			if (parsed && parsed !== currentValue) {
+				onChangeText && onChangeText(parsed)
+				setCurrentValue(parsed)
+			}
+		},
+		[currentValue]
+	)
+
+	const handleOnBlur = useCallback(() => {
+		const formattedNumber = toDisplayNumber(fieldValue)
+
+		setError(
+			fieldValue === "" || formattedNumber
+				? null
+				: t("errors:invalidPhoneNumber")
+		)
+		setFieldValue(formattedNumber || fieldValue)
+	}, [fieldValue])
+
+	useEffect(() => {
+		if (value !== currentValue) {
+			setCurrentValue(value)
+			handleOnBlur()
+		}
+	}, [value, currentValue])
+
+	console.log("error is", error)
 
 	return (
 		<TextField
 			keyboardType="phone-pad"
 			textContentType="telephoneNumber"
 			autocompletetype="tel"
-			value={isValid ? toDisplayNumber(value) : value || ""}
-			error={error || props.error}
-			onChangeText={onChangeText}
-			onFocus={handleOnFocus}
-			onBlur={handleOnBlur}
 			{...nextProps}
+			value={fieldValue}
+			error={error || props.error}
+			onChangeText={handleChange}
+			onBlur={handleOnBlur}
+			after={
+				<>
+					{StatusIcon[status]}
+					{after}
+				</>
+			}
 		/>
 	)
+}
+
+export function toInternational(number) {
+	if (!number) return
+	const parsed = parsePhoneNumberFromString(number, "CA")
+	return parsed && parsed.number
+}
+
+export function toDisplayNumber(number) {
+	if (!number) return
+	const parsed = parsePhoneNumberFromString(number, "CA")
+	return parsed && parsed.formatNational()
 }
