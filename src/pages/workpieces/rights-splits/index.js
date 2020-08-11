9e63004e20b10c6c76b4cf0d5f6277486039ba7a
Pandassi <lila.ait.ab@gmail.com>
2020-08-10 20:26:17 -0400

    En cours

--- src/pages/workpieces/rights-splits/index.js
+++ src/pages/workpieces/rights-splits/index.js
@@ -1,6 +1,18 @@
 import React from "react"
+import { Switch, Route } from "react-router"
 import CopyrightPage from "./copyright"
+import DocumentCreation from "../documentation/document-creation"
 
 export default function (props) {
-	return <CopyrightPage {...props} />
+	return (
+		<Switch>
+			<Route path="/workpieces/page1">
+				<CopyrightPage {...props} />
+			</Route>
+
+			<Route path="/documentation/document-creation">
+				<DocumentCreation />
+			</Route>
+		</Switch>
+	)
 }
