730a93001112bedeaef8a46ef67ff53237c04820
Pandassi <lila.ait.ab@gmail.com>
2020-10-27 17:59:46 -0400

    en cours

--- src/pages/workpieces/documentation/lyrics.js
+++ src/pages/workpieces/documentation/lyrics.js
@@ -9,114 +9,83 @@ import Button from "../../../widgets/button"
 import { Column, Row, Flex, Hairline, Spacer } from "../../../layout"
 import { Text, Heading, Paragraph } from "../../../text"
 import { Colors, Metrics } from "../../../theme"
-import LyricsIcon from "../../../svg/lyrics"
+import LyricsIcon from "../../../svg/feather"
 import EyeIcon from "../../../svg/eye"
-import { Dropdown, TextField } from "../../../forms"
-import AddLanguageDropdown from "../../../smartsplit/components/add-language-dropdown"
+import { SearchAndTag, Dropdown, TextField } from "../../../forms"
 import { TextInput } from "react-native"
-
-export default function Lyrics() {
-	const { t } = useTranslation()
-	const history = useHistory()
-	const workpiece = useCurrentWorkpiece()
-
-	function saveAndQuit() {
-		history.push("/dashboard/")
-	}
-
-	function navigateToSummary() {
-		history.push(`/workpieces/${workpiece.id}`)
-	}
-
-	function navigateToInterpretation() {
-		history.push(`/workpieces/${workpiece.id}/rights-splits/interpretation`)
-	}
-
-	return (
-		<Layout
-			workpiece={workpiece}
-			title={workpiece}
-			path={[t("document:navbar.document"), t("document:navbar.pages.lyrics")]}
-			progress={62.5}
-			actions={
-				<Button
-					tertiary
-					text={t("general:buttons.saveClose")}
-					onClick={saveAndQuit}
-				/>
-			}
-			formNav={
-				<>
-					<Row flex={1}>
-						<Button
-							secondary
-							text={t("general:buttons.back")}
-							onClick={navigateToSummary}
-						/>
-						<Flex />
-						<Button
-							primary
-							text={t("general:buttons.pass")}
-							onClick={navigateToInterpretation}
-						/>
-					</Row>
-					<Row flex={1} />
-				</>
-			}
-		>
-			<LyricsForm />
-		</Layout>
-	)
-}
 
 const formStyle = StyleSheet.create({
 	textAreaContainer: {
-		padding: 5,
+		/* 		padding: 5,
 		borderWidth: 1,
-		borderRadius: 2,
+		borderRadius: 2, */
 		borderColor: Colors.stroke,
 		minHeight: 264,
+		resize: "vertical",
+		/* 		"&::-webkitResizer": {
+			background: Colors.stroke,
+		}, */
 	},
 	textArea: {
 		justifyContent: "flex-start",
 	},
 })
 
+const Styles = StyleSheet.create({
+	category: {
+		alignItems: "center",
+		display: "flex",
+	},
+	logo: {
+		marginRight: Metrics.spacing.medium,
+	},
+	dropdown: {
+		marginLeft: Metrics.spacing.large,
+	},
+})
+
 export function LyricsForm(props) {
-	const { t } = useTranslation()
-	const searchResults = [
-		t("document:lyrics.frenchCanadian"),
-		t("document:lyrics.french"),
-	]
-	const [selected, setSelected] = useState(["English", "Français"])
+	const searchResults = ["Electrofunk", "Future Funk", "Mega Funk"]
 	const [search, setSearch] = useState("")
+	const [selected, setSelected] = useState(["English", "Français"])
+	const { t } = useTranslation()
 	const [text, setText] = React.useState("")
 
 	return (
 		<>
 			<Row>
 				<Column of="group" flex={5}>
-					<Text action bold valign="center">
-						<LyricsIcon />
+					<Text action bold style={Styles.category}>
+						<LyricsIcon style={Styles.logo} />
 						{t("document:infos.category")}
 						<Row padding="tiny" />
 					</Text>
 					<Heading level={1}>{t("document:infos.title")}</Heading>
 
 					<Spacer of="group" />
-					<TextInput
-						label={t("document:lyrics.lyrics")}
+
+					<TextField
+						label={t("document:lyrics.label")}
+						value={text}
+						//multiline={true}
+						style={formStyle.textAreaContainer}
+						onChangeText={(text) => setText(text)}
+						undertext={t("document:release.lyrics.undertext")}
+					/>
+
+					{/* 					<TextInput
+						label="Text"
 						value={text}
 						multiline={true}
 						style={formStyle.textAreaContainer}
 						onChangeText={(text) => setText(text)}
-					/>
-					<AddLanguageDropdown
+						undertext={t("document:release.lyrics.undertext")}
+					/> */}
+
+					<SearchAndTag
 						hideIcon={true}
 						label={t("document:lyrics.language")}
-						searchResults={searchResults.filter((v) =>
-							v ? v.toLowerCase().indexOf(search.toLowerCase()) > -1 : true
-						)}
+						searchResults={searchResults}
 						search={search}
 						onSearchChange={setSearch}
 						selection={selected}
@@ -127,12 +96,11 @@ export function LyricsForm(props) {
 						placeholder={t("document:lyrics.addLanguage")}
 					/>
 					<Dropdown
-						hideIcon={true}
 						label={t("document:access")}
 						placeholder={
 							<>
+								{/* {t("document:lyrics.propdown.public")} */}
 								<EyeIcon />
-								{/* {t("document:lyrics.public")} */}
 								<Flex />
 							</>
 						}
