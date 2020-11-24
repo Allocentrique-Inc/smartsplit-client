a77dcac36706e9610fc05d4330b618630af1fce7
will <will.eizlini@gmail.com>
2020-11-24 17:48:07 -0500

    added exported instrument list (while awaiting db update)

--- assets/listgen.js
+++ assets/listgen.js
@@ -1,2 +1,43 @@
 const fs = require("fs")
-const realine = require("readline")
+const lineReader = require("line-reader")
+let instruments = []
+lineReader.eachLine("instruments-musz.json", (line, last) => {
+	let ins = JSON.parse(line)
+	//if (ins.description && !last) return
+	let instrument = {
+		name: ins.name,
+		muszId: ins.id,
+		description: ins.description,
+		type: ins.type,
+	}
+	if (ins.relations) {
+		ins.relations.forEach((relation) => {
+			switch (relation.type) {
+				case "wikidata":
+					instrument.wikidata = relation.url
+					break
+			}
+		})
+	}
+	let lang = { en: instrument.name }
+	if (ins.aliases) {
+		ins.aliases.forEach((alias) => {
+			if (alias.locale) lang[alias.locale] = alias.name
+		})
+	}
+	instrument.lang = lang
+	instruments.push(instrument)
+	if (last) {
+		instruments.sort((a, b) => (a.name < b.name ? -1 : 1))
+		try {
+			fs.writeFileSync(
+				"instruments-smartsplit.json",
+				JSON.stringify(instruments, null, 2)
+			)
+		} catch (err) {
+			console.error(err)
+		}
+		console.log(`processed ${instruments.length} instruments`)
+		//console.log(JSON.stringify(ins, null, 2))
+	}
+})
