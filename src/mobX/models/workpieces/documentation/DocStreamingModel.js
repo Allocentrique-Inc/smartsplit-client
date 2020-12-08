ff6f93eea40003cc696156b002b271b2a13dc58b
will <will.eizlini@gmail.com>
2020-12-08 17:11:18 -0500

    added default value

--- src/mobX/models/workpieces/documentation/DocStreamingModel.js
+++ src/mobX/models/workpieces/documentation/DocStreamingModel.js
@@ -1,7 +1,46 @@
 import BaseModel, { FieldType, Field } from "../../../BaseModel"
 import { observable, action, computed } from "mobx"
+import PandoraIcon from "../../../../svg/workpieces/pandora"
+import SoundcloudIcon from "../../../../svg/workpieces/soundcloud"
+import DeezerIcon from "../../../../svg/workpieces/deezer"
+import YoutubeIcon from "../../../../svg/workpieces/youtube"
+import SpotifyIcon from "../../../../svg/workpieces/spotify"
+import ITunesIcon from "../../../../svg/workpieces/itunes"
+import AmazonIcon from "../../../../svg/workpieces/amazon"
+import GooglePlayIcon from "../../../../svg/workpieces/google-play"
 export default class DocStreamingModel extends BaseModel {
-	@observable links = new Field(this, "links", { type: FieldType.map })
+	@observable links = new Field(this, "links", {
+		type: FieldType.map,
+		default: {
+			spotify: "",
+			google: "",
+			apple: "",
+			amazon: "",
+			youtube: "",
+			pendora: "",
+			soundcloud: "",
+			deezer: "",
+		},
+	})
+	importData(obj) {
+		if (!obj) return null
+		let linksNormalized = {
+			spotify: "",
+			google: "",
+			apple: "",
+			amazon: "",
+			youtube: "",
+			pendora: "",
+			soundcloud: "",
+			deezer: "",
+		}
+		console.log(linksNormalized)
+		if (obj && obj.links)
+			obj.links.forEach((link) => {
+				linksNormalized[link.platform] = link.url
+			})
+		return { links: linksNormalized }
+	}
 	toJS() {
 		let values = super.toJS().links
 		let links = []
