cba93debb4bbe760272d482189a3266432b51aa7
will <will.eizlini@gmail.com>
2021-01-25 13:07:30 -0500

    modifications to DocRecordingModel.js for proper page loading

--- src/pages/workpieces/documentation/recording.js
+++ src/pages/workpieces/documentation/recording.js
@@ -28,13 +28,16 @@ import searchResultsStudio from "../../../data/studios-smartsplit"
 import { FormStyles } from "./FormStyles"
 import AddCollaboratorDropdown from "../../../smartsplit/components/AddCollaboratorDropdown"
 import DatePickers from "../../../smartsplit/components/DatePickers"
+import PlusCircle from "../../../svg/plus-circle"
+import { TouchableWithoutFeedback } from "react-native"
 
 const RecordingForm = observer((props) => {
 	//const searchResults = ["Inie", "Manie", "Moe"]
 	const [search, setSearch] = useState("")
 	const [date, setDate] = useState("")
-	const { t } = useTranslation()
-
+	const { t, i18n } = useTranslation()
+	const quotation = i18n.language === "en" ? '"' : "« "
+	const quotationEnd = i18n.language === "en" ? '"' : " »"
 	const workpiece = useCurrentWorkpiece()
 	const workpieceId = workpiece.id
 	const { contributors } = useStores()
@@ -231,21 +234,15 @@ const RecordingForm = observer((props) => {
 					<SearchAndTag
 						noIcon={true}
 						label={t("document:recording.studio")}
-						searchResults={searchResultsStudio
-							.filter(
-								(g) =>
-									g.name.toLowerCase().indexOf(searchStudio.toLowerCase()) > -1
-							)
-							.splice(0, 10)}
+						searchResults={[]}
 						search={searchStudio}
 						onSearchChange={setSearchStudio}
 						selection={model.recordingStudio.array}
 						onSelect={
 							(selection) => {
 								let exists =
-									model.recordingStudio.array.filter(
-										(g) => g.id === selection.id
-									).length > 0
+									model.recordingStudio.array.filter((g) => g === selection)
+										.length > 0
 								if (!exists) model.recordingStudio.add(selection)
 							}
 							//setSelectedStudio([...selectedStudio, selection])
@@ -256,7 +253,30 @@ const RecordingForm = observer((props) => {
 						}
 						placeholder={t("document:recording.searchStudio")}
 						tooltip=""
-					/>
+					>
+						<TouchableWithoutFeedback
+							onPress={() => {
+								let exists =
+									model.recordingStudio.array.filter((g) => g === searchStudio)
+										.length > 0
+								if (!exists) model.recordingStudio.add(searchStudio)
+							}}
+						>
+							<Row
+								of="component"
+								padding="component"
+								//style={FormStyles.actionFrame}
+							>
+								<PlusCircle />
+								<Text bold action>
+									{t("document:add")}
+									{searchStudio ? quotation : null}
+									{searchStudio}
+									{searchStudio ? quotationEnd : null}
+								</Text>
+							</Row>
+						</TouchableWithoutFeedback>
+					</SearchAndTag>
 					{/* <TextField
 					name="studio"
 					label={t("document:recording.studio")}
